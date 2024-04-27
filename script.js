2dd68aca5310f0b9bc04f90bc7d371c3be2f1910
Alimur Razi <138320899+alimurdipu@users.noreply.github.com>
2024-04-28 00:43:49 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    //   setting-name: setting-value
+    // fade: true,
+    // cssEase: 'linear',
+
+    // centerMode: true,
+    // centerPadding: "60px",
+    // slidesToShow: 1,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+
+    // autoplay: false,
+    // autoplayspeed: 100,
+    arrows: true,
+    prevArrow: ".back",
+    nextArrow: ".next",
+    dots: true,
+    // slidesToShow: 3,
+    // slidesToScroll: 1,
+  });
+});
